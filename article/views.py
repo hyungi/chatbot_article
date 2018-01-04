0e6cdeee3be76d77696ead7355da6bb373c0a027
hyeonik7 <hyeonik7@gmail.com>
2018-01-04 10:28:33 +0900

    urls update

--- article/views.py
+++ article/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
+ 
+def keyboard(request):
+ 
+        return JsonResponse({
+                'type' : 'buttons',
+                'buttons' : ['1','2']
+                })
+ 
+@csrf_exempt
+def message(request):
+        message = ((request.body).decode('utf-8'))
+        return_json_str = json.loads(message)
+        return_str = return_json_str['content']
+ 
+        return JsonResponse({
+                'message': {
+                        'text': "you type "+return_str+"!"
+                },
+                'keyboard': {
+                        'type': 'buttons',
+                        'buttons': ['1','2']
+                }
+        })
 
-# Create your views here.
