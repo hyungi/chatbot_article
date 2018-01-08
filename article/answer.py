3e26c0c1f505210bd6eb4edaf779cd8cc7ddb6c8
hyeonik7 <hyeonik7@gmail.com>
2018-01-08 19:19:06 +0900

    global_variable_set3

--- article/answer.py
+++ article/answer.py
@@ -5,9 +5,9 @@ from article.lists import *
 from article.returns import *
 #lists 의 모든 리스트는 set자료형임
 
-press=None
-date=None
-category=None
+press=""
+date=""
+category=""
 
 @csrf_exempt
 def message(request):
@@ -24,8 +24,31 @@ def message(request):
     return_json_str = json.loads(message)
     content = return_json_str['content']
     user_key = return_json_str['user_key']
+    
+    
+    isPress = check_is_in_presslist(content)
+    isDate = check_is_in_datelist(content)
+    isCategory = check_is_in_categorylist(content)
+    
     #요청하기가 들어오면 다른 .py 파일에서 불러온 기사 요약 정보를 보여줄 수 있도록 하자. 
-    if content == u"신문사 고르기":
+    
+    if is_Full():
+        result = ""
+        result = press +", "+date+", "+category
+        press = ""
+        date = ""
+        catgory = ""
+        return JsonResponse({
+            'message':{
+                'text':'선택이 모두 완료되었습니다.'
+                },
+            'keyboard':{
+                'type':'buttons',
+                'buttons':list(menulist)
+                }
+            })
+
+    elif content == u"신문사 고르기":
         return JsonResponse({
             'message': {
                 'text': "신문사를 골라주세요!"
@@ -55,7 +78,7 @@ def message(request):
                 'buttons' : list(categorylist)
                 }
             })
-    elif content in presslist:
+    elif isPress:
         press = content
         return JsonResponse({
             'message': {
@@ -66,7 +89,7 @@ def message(request):
             'buttons' : list(menulist)
                 }
             })
-    elif content in datelist:
+    elif isDate:
         date = content
         return JsonResponse({
             'message': {
@@ -77,7 +100,7 @@ def message(request):
             'buttons' : list(menulist)
                 }
             })   
-    else:
+    elif isCategory:
         category = content
         return JsonResponse({
             'message': {
@@ -87,4 +110,45 @@ def message(request):
             'type': 'buttons',
             'buttons' : list(menulist)
                 }
-            })     
+            }) 
+    else :
+        return JsonResponse({
+            'message': {'text':'죄송합니다 정의되지 않은 응답입니다.'},
+            'keyboard' : {
+                'type':'buttons',
+                'buttons' : list(menulist)
+                }
+            })
+#신문사 이름중 하나인지 확인
+def check_is_in_presslist(content):
+    if content in presslist:
+        return True
+    else:
+        return False
+#날짜 목록중 하나인지 체크 -- 임시방편이라 수정해야함
+def check_is_in_datelist(content):
+    if content in datelist:
+        return True
+    else:
+        return False
+#카테고리 중 하나인지 체크
+def check_is_in_categorylist(content):
+    if content in categorylist:
+        return True
+    else:
+        return False
+#전부다 선택했는지 확인
+def is_Full():
+    global press
+    global date
+    global category
+    
+    if press is None:
+        return False
+    elif date is None:
+        return False
+    elif category is None:
+        return False
+    else:
+        return True
+
