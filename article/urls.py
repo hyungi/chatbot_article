8a8cda4286f7f5b06b213cdd4c7683d954ffd245
hyeonik7 <hyeonik7@gmail.com>
2018-01-08 20:41:27 +0900

    selcet.py_added_again2

--- article/urls.py
+++ article/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 from django.contrib import admin
 from . import views, answer
-from article.select import *
+from .select import *
 
 
 selector = select()
@@ -10,7 +10,7 @@ urlpatterns = [
         
         url(r'^keyboard/',views.keyboard),
 #        url(r'^message',answer.message),
-        url(r'^message',select.message),
+        url(r'^message',selector.message),
         url(r'^admin/',admin.site.urls),
         url(r'^friend$', views.add_friend),
         url(r'^friend/(?P<user_key>\w+)$', views.del_friend)
