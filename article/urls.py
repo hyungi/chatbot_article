11dcdd29dba0f46462dd166a6ca47ad4be403011
hyeonik7 <hyeonik7@gmail.com>
2018-01-08 16:17:10 +0900

    add_select.py

--- article/urls.py
+++ article/urls.py
@@ -1,11 +1,15 @@
 from django.conf.urls import url
 from django.contrib import admin
 from . import views, answer
+from article.select import *
+
+selector = select()
 
 urlpatterns = [
         
         url(r'^keyboard/',views.keyboard),
-        url(r'^message',answer.message),
+#        url(r'^message',answer.message),
+        url(r'^message',selector.message),
         url(r'^admin/',admin.site.urls),
         url(r'^friend$', views.add_friend),
         url(r'^friend/(?P<user_key>\w+)$', views.del_friend)
