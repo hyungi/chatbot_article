779b9cc2a9a984a87b435d0e95db5a460f438bf1
hyeonik7 <hyeonik7@gmail.com>
2018-01-17 16:59:54 +0900

    WSGI_problem

---
+++ management/commands/createsu.py
@@ -0,0 +1,9 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.contrib.auth.models import User
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        if not User.objects.filter(username="hyungi").exists():
+            User.objects.create_superuser("hyungi", "hyeonik7@gmail.com", "hyungi")
+            self.stdout.write(self.style.SUCCESS('Successfully created new super user'))
